2c56daa8b312000f77763534480935a0a55ebd60
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-11-07 22:09:15 +0530

    project initialize with dependencies.

---
+++ src/main/java/lk/ijse/crop_monitoring_systembackend/CropMonitoringSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.crop_monitoring_systembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitoringSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CropMonitoringSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/crop_monitoring_systembackend/CropMonitoringSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.crop_monitoring_systembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitoringSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
