6749ee43d5c48b99e080c7a5d17aa3b612300b4e
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-11-10 14:27:54 +0530

    staff entity dao layer implemented.

---
+++ src/main/java/lk/ijse/crop_monitoring_systembackend/dao/StaffDAO.java
@@ -0,0 +1,5 @@
+package lk.ijse.crop_monitoring_systembackend.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StaffDAO extends JpaRepository<StaffDAO, String> {}
