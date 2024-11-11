657a7bbd83e385da8e9acb361dd47a4f4a01de57
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-11-11 17:47:46 +0530

    jwt service & authentication service impl classes implemented.

---
+++ src/main/java/lk/ijse/crop_monitoring_systembackend/service/impl/AuthenticationServiceIMPL.java
@@ -0,0 +1,47 @@
+package lk.ijse.crop_monitoring_systembackend.service.impl;
+
+import lk.ijse.crop_monitoring_systembackend.dao.UserDAO;
+import lk.ijse.crop_monitoring_systembackend.dto.UserDTO;
+import lk.ijse.crop_monitoring_systembackend.jwtModel.JWTAuthResponse;
+import lk.ijse.crop_monitoring_systembackend.jwtModel.SignIn;
+import lk.ijse.crop_monitoring_systembackend.service.AuthenticationService;
+import lk.ijse.crop_monitoring_systembackend.service.JWTService;
+import lk.ijse.crop_monitoring_systembackend.util.MappingUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceIMPL implements AuthenticationService {
+    private final UserDAO userDAO;
+    private final JWTService jwtService;
+    private final MappingUtil mapping;
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public JWTAuthResponse signIn(SignIn signIn) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signIn.getEmail(),signIn.getPassword()));
+        var userByEmail = userDAO.findByEmail(signIn.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        var generatedToken = jwtService.generateToken(userByEmail);
+        return JWTAuthResponse.builder().token(generatedToken).build() ;
+    }
+
+    @Override
+    public JWTAuthResponse signUp(UserDTO signUpUser) {
+        var savedUser = userDAO.save(mapping.userConvertToEntity(signUpUser));
+        var genToken = jwtService.generateToken(savedUser);
+        return JWTAuthResponse.builder().token(genToken).build();
+    }
+
+    @Override
+    public JWTAuthResponse refreshToken(String accessToken) {
+        var userName = jwtService.extractUsername(accessToken);
+        var userEntity =
+                userDAO.findByEmail(userName).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        var refreshToken = jwtService.refreshToken(userEntity);
+        return JWTAuthResponse.builder().token(refreshToken).build();
+    }
+}
