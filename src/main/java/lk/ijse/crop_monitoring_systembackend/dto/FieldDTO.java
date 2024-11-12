f0cbc3af76f71bc632d05c05edae00cea631169a
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-11-12 10:46:08 +0530

    field dto validated using javax validation.

--- src/main/java/lk/ijse/crop_monitoring_systembackend/dto/FieldDTO.java
+++ src/main/java/lk/ijse/crop_monitoring_systembackend/dto/FieldDTO.java
@@ -1,5 +1,8 @@
 package lk.ijse.crop_monitoring_systembackend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lk.ijse.crop_monitoring_systembackend.customResponse.Response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,11 +17,18 @@ import java.util.List;
 @Data
 public class FieldDTO implements Serializable, Response {
     private String fieldId;
+    @NotBlank(message = "Field name cannot be blank")
+    @Size(max = 100, message = "Field name must be at most 100 characters")
     private String fieldName;
+    @NotNull(message = "Location cannot be null")
     private Point location;
+    @NotBlank(message = "Size cannot be blank")
+    @Size(max = 20, message = "Size must be at most 20 characters")
     private String size;
+    @Size(max = 10485760, message = "Image 1 size exceeds maximum allowed length")
     private String fieldImg1;
+    @Size(max = 10485760, message = "Image 2 size exceeds maximum allowed length")
     private String fieldImg2;
-    private List<CropDTO> crops;
-    private List<StaffDTO> staff;
+    private List<@NotNull(message = "Crop list contains null elements") CropDTO> crops;
+    private List<@NotNull(message = "Staff list contains null elements") StaffDTO> staff;
 }
