ba48d0f9100fbc52506e69c6bb281b19b47592f9
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-11-12 20:54:08 +0530

    Equipment update method implemented.

--- src/main/java/lk/ijse/crop_monitoring_systembackend/controller/EquipmentController.java
+++ src/main/java/lk/ijse/crop_monitoring_systembackend/controller/EquipmentController.java
@@ -3,16 +3,14 @@ package lk.ijse.crop_monitoring_systembackend.controller;
 import jakarta.validation.Valid;
 import lk.ijse.crop_monitoring_systembackend.dto.EquipmentDTO;
 import lk.ijse.crop_monitoring_systembackend.exception.DataPersistFailedException;
+import lk.ijse.crop_monitoring_systembackend.exception.NotFoundException;
 import lk.ijse.crop_monitoring_systembackend.service.EquipmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
@@ -36,6 +34,27 @@ public class EquipmentController {
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }catch (Exception e){
                 logger.severe("Failed to save equipment: " + equipment);
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> updateEquipment(@Valid @PathVariable("id") String id, @RequestBody EquipmentDTO equipment) {
+        if (id != null && equipment != null) {
+            try {
+                equipmentService.updateEquipment(id, equipment);
+                logger.info("Equipment updated successfully: " + equipment);
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } catch (NotFoundException e) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } catch (DataPersistFailedException e) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            } catch (Exception e){
+                logger.severe("Failed to update equipment: " + equipment);
                 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
--- src/main/java/lk/ijse/crop_monitoring_systembackend/service/impl/EquipmentServiceIMPL.java
+++ src/main/java/lk/ijse/crop_monitoring_systembackend/service/impl/EquipmentServiceIMPL.java
@@ -2,6 +2,8 @@ package lk.ijse.crop_monitoring_systembackend.service.impl;
 
 import lk.ijse.crop_monitoring_systembackend.dao.EquipmentDAO;
 import lk.ijse.crop_monitoring_systembackend.dto.EquipmentDTO;
+import lk.ijse.crop_monitoring_systembackend.entity.EquipmentEntity;
+import lk.ijse.crop_monitoring_systembackend.exception.NotFoundException;
 import lk.ijse.crop_monitoring_systembackend.service.EquipmentService;
 import lk.ijse.crop_monitoring_systembackend.util.MappingUtil;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -30,7 +33,18 @@ public class EquipmentServiceIMPL implements EquipmentService {
 
     @Override
     public void updateEquipment(String id, EquipmentDTO equipment) {
-
+        Optional<EquipmentEntity> tmpEquipment = equipmentDAO.findById(id);
+        if (tmpEquipment.isPresent()) {
+            EquipmentEntity equipmentEntity = mappingUtil.equipmentConvertToEntity(equipment);
+            tmpEquipment.get().setCategory(equipmentEntity.getCategory());
+            tmpEquipment.get().setStatus(equipmentEntity.getStatus());
+            tmpEquipment.get().setType(equipmentEntity.getType());
+            tmpEquipment.get().setStaff(equipmentEntity.getStaff());
+            tmpEquipment.get().setField(equipmentEntity.getField());
+            System.out.println("Equipment updated successfully: " + tmpEquipment.get());
+        } else {
+            throw new NotFoundException("Equipment not found with id: " + id);
+        }
     }
 
     @Override
